9f725ba7bf2e32ba86be65fd9db6843e3d122302
ayroh <jakubjanlesniewski@gmail.com>
2023-04-16 11:54:36 +0200

    minor refactoring

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,21 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnsCloseModal = document.querySelectorAll('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-const OpenModal = function () {
+const openModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-const CloseModal = function () {
+
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', function () {
-    OpenModal();
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
 
-  btnsCloseModal[i].addEventListener('click', function () {
-    CloseModal();
-  });
+  btnsCloseModal[i].addEventListener('click', closeModal);
 }
