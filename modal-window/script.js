22e73481bbfb5438624801967a57fd614a05af95
ayroh <jakubjanlesniewski@gmail.com>
2023-04-16 12:24:02 +0200

    finished modal project

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,18 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnsCloseModal = document.querySelectorAll('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-const openModal = () => {
+function openModal() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
+}
 
-const closeModal = () => {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-};
+}
 
 for (let i = 0; i < btnsShowModal.length; i++) {
   btnsShowModal[i].addEventListener('click', openModal);
 
-  btnsCloseModal[i].addEventListener('click', closeModal);
+  if (btnsCloseModal[i]) {
+    btnsCloseModal[i].addEventListener('click', closeModal);
+  }
 }
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
