ec273b2554a84659862717fbce2b364c19b1587e
ayroh <jakubjanlesniewski@gmail.com>
2023-04-16 11:46:54 +0200

    initial logic for modal window

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelectorAll('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const OpenModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const CloseModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    OpenModal();
+  });
+
+  btnsCloseModal[i].addEventListener('click', function () {
+    CloseModal();
+  });
+}
