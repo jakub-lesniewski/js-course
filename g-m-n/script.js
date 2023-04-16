b147f6b6a4489acecf4787806f1389e27abee4e9
ayroh <jakubjanlesniewski@gmail.com>
2023-04-16 10:29:38 +0200

    added reset logic

--- g-m-n/script.js
+++ g-m-n/script.js
@@ -1,17 +1,23 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1; // state variable
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20; // state variable
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = parseInt(document.querySelector('.guess').value);
 
+  // no input
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number!';
+
+    // player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+
+    // guess too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -20,6 +26,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lost!';
     }
+
+    //guess too low
   } else {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
@@ -29,4 +37,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost!';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
 });
