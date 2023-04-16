67e05301df40c695e67b804ac5036eb04df1cd72
ayroh <jakubjanlesniewski@gmail.com>
2023-04-16 10:07:10 +0200

    initial logic

--- g-m-n/script.js
+++ g-m-n/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // state variable
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20; // state variable
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = parseInt(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!';
+    }
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!';
+    }
+  }
+});
